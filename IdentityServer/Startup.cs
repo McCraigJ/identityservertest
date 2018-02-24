eb64b3366435dad87858c357a6cbd39515351398
unknown <mccraigj@gmail.com>
2018-02-24 17:33:24 +0000

    Incomplete http://docs.identityserver.io/en/release/quickstarts/1_client_credentials.html

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,7 +16,9 @@ namespace IdentityServer
       services.AddIdentityServer()
         //.AddInMemoryClients()
         // Creates a temporary key material for development
-        .AddDeveloperSigningCredential();
+        .AddDeveloperSigningCredential()
+        .AddInMemoryApiResources(Config.GetApiResources())
+        .AddInMemoryClients(Config.GetClients());
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
